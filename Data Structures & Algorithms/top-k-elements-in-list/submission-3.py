cacc0a3ebe47a0c8bd8f2eafc8dd1b401f92cef3
Ninad Sharma <52912195+ninadsharma@users.noreply.github.com>
2026-06-05 23:49:38 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #freqGraph = {x: nums.count(x) for x in nums}  
+        #print(freqGraph)
+        sortedGraph = dict(sorted({x: nums.count(x) for x in nums}.items(), key = lambda item: item[1], reverse = True))
+        #print(sortedGraph)
+        returnList = []
+        for i in range(k):
+            returnList.append(list(sortedGraph)[i])
+        return returnList
